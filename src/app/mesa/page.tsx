342729ce8961b7a5f4dbc705b7057233e687b5a8
Rich Johnson <webaddict@gmail.com>
2025-02-19 05:28:11 +0000

    Don't open links to new window

--- src/app/mesa/page.tsx
+++ src/app/mesa/page.tsx
@@ -17,7 +17,6 @@ export default function Page() {
       <a
         href="/mesa"
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        target="_blank"
         rel="noopener noreferrer"
       >
         <h2 className="mb-3 text-2xl font-semibold">
@@ -34,7 +33,6 @@ export default function Page() {
       <a
         href="/tucson"
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        target="_blank"
         rel="noopener noreferrer"
       >
         <h2 className="mb-3 text-2xl font-semibold">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,6 @@ export default function Home() {
         <a
           href="/mesa"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
@@ -35,7 +34,6 @@ export default function Home() {
         <a
           href="/tucson"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
