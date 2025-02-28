c35292bd9f4840819a4835c6abad12166832bc06
Denver Lambson <dglambson@gmail.com>
2025-02-27 21:24:01 -0700

    logos and music page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
+"use client"
+
 import Navbar from "@/components/navbar/Navbar";
-import Image from "next/image";
-import Link from 'next/link';
+import { Stream } from "@cloudflare/stream-react";
 
 export default function Home() {
+  const videoID = "https://customer-6dmg386fdvg78j60.cloudflarestream.com/47c231680d57feba393dd3c7c71b38e0/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-6dmg386fdvg78j60.cloudflarestream.com%2F47c231680d57feba393dd3c7c71b38e0%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
     <Navbar />
-    <div className="relative pt-[56.25%]">
-    <iframe
-        src="https://customer-6dmg386fdvg78j60.cloudflarestream.com/47c231680d57feba393dd3c7c71b38e0/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-6dmg386fdvg78j60.cloudflarestream.com%2F47c231680d57feba393dd3c7c71b38e0%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
-        loading="lazy"
-        className="absolute top-0 left-0 w-full h-full border-none"
-        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture"
-        allowFullScreen={true}
-      />
-    </div>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <div className="text-center"><em className="mb-3 text-2xl font-semibold">Strive To Be</em><br /> is coming to ARIZONA</div>
       </div>
@@ -51,7 +44,6 @@ export default function Home() {
           Learn about the Concert in Tucson on March 29th
           </p>
         </a>
-
       </div>
     </main>
   );
--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -36,13 +36,18 @@ export default function Register({ event }: { event: string }) {
     setEmailError('');
     setMobileError('');
 
+    if (!userRegistration.email || !userRegistration.firstName || !userRegistration.lastName || !userRegistration.age || !userRegistration.religion) {
+      setError("Missing required fields");
+      return;
+    }
+
     if (!validateEmailInput(userRegistration.email)) {
       setEmailError('Invalid email');
       setLoading(false);
       return;
     }
 
-    if (!validatePhoneInput(userRegistration.mobile)) {
+    if (userRegistration.mobile.length > 0 && !validatePhoneInput(userRegistration.mobile)) {
       setMobileError('Invalid phone');
       setLoading(false);
       return;
@@ -81,6 +86,7 @@ export default function Register({ event }: { event: string }) {
               minWidth: '250px',
             },
           });
+          setError(data.error);
         } else {
           throw new Error('Registration failed with an unknown error');
         }
@@ -102,7 +108,7 @@ export default function Register({ event }: { event: string }) {
       <div className={styles.form}>
         <input
           type="text"
-          placeholder="First Name"
+          placeholder="First Name*"
           name="firstName"
           value={userRegistration.firstName}
           onChange={handleInputChange}
@@ -111,7 +117,7 @@ export default function Register({ event }: { event: string }) {
         />
         <input
           type="text"
-          placeholder="Last Name"
+          placeholder="Last Name*"
           name="lastName"
           value={userRegistration.lastName}
           onChange={handleInputChange}
@@ -120,7 +126,7 @@ export default function Register({ event }: { event: string }) {
         />
         <input
           type="email"
-          placeholder="Email"
+          placeholder="Email*"
           name="email"
           value={userRegistration.email}
           onChange={handleInputChange}
@@ -145,7 +151,7 @@ export default function Register({ event }: { event: string }) {
           className={styles.select}
           required
         >
-        <option value="" disabled>Religion</option>
+        <option value="" disabled>Religion*</option>
         <option value="LDS">LDS</option>
         <option value="other">Other</option>
       </select>
@@ -157,8 +163,12 @@ export default function Register({ event }: { event: string }) {
         className={styles.select}
         required
       >
-        <option value="" disabled>Age</option>
-        <option value="14-18">14-18</option>
+        <option value="" disabled>Age*</option>
+        <option value="14">14</option>
+        <option value="14">15</option>
+        <option value="14">16</option>
+        <option value="14">17</option>
+        <option value="14">18</option>
         <option value="adult">Adult</option>
       </select>
         <button onClick={handleSubmit} disabled={loading} className={styles.button}>
